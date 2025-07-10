b149f9cd954ab24b8c404561172dec734a17ee71
fahad <jtfahadhassan@gmail.com>
2025-07-11 01:48:08 +0500

    Added default route as /chatFreemium

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    domains: ["placehold.co"],
   },
-  images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/chatFreemium',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
